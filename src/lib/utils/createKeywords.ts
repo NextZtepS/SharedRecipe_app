976e9b83d67e6b59de803f8bce1dd94dd8d914dc
NextZtepS <natdanaiongarjvaja@gmail.com>
2023-08-11 09:46:31 +0700

    update createKeywords.ts

--- src/lib/utils/createKeywords.ts
+++ src/lib/utils/createKeywords.ts
@@ -7,16 +7,20 @@ export function createKeywords(
     menuName = menuName.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     userName = userName.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     if (menuName)
-    for (let i = 1; i <= menuName.length; i++) {
-        keywords.push(menuName.substring(0, i))
-    }
+        for (let i = 1; i <= menuName.length; i++) {
+            keywords.push(menuName.substring(0, i))
+        }
     if (userName)
-    for (let i = 1; i <= userName.length; i++) {
-        keywords.push(userName.substring(0, i))
-    }
-    if (tagList.length != 0)
-    for (let tag of tagList) {
-        keywords.push(tag.replace(/[^a-zA-Z0-9]/g, "").toLowerCase());
+        for (let i = 1; i <= userName.length; i++) {
+            keywords.push(userName.substring(0, i))
+        }
+    if (tagList.length != 0) {
+        for (let tag of tagList) {
+            tag = tag.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+            for (let i = 1; i <= userName.length; i++) {
+                keywords.push(tag.substring(0, i));
+            }
+        }
     }
     return keywords;
 }
