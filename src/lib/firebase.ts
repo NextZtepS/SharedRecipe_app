aeaf21a59ec8378ed1dd3d0f54866e32fee0128f
NextZtepS <natdanaiongarjvaja@gmail.com>
2023-08-11 22:55:16 +0700

    update app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "@firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -8,13 +8,8 @@ import { writable } from "svelte/store";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIy8E1dPDKIoJojTlmfMj5A5X-dLfDyU8",
-    authDomain: "shared-recipe.firebaseapp.com",
-    projectId: "shared-recipe",
-    storageBucket: "shared-recipe.appspot.com",
-    messagingSenderId: "592985192315",
-    appId: "1:592985192315:web:670e12f4bd230f01ebafde",
-    measurementId: "G-WZR4VRD285"
+    // your app configuration
+    //...
 };
 
 // Initialize Firebase
