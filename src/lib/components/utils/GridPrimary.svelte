3a658578a5f6d5b6d448673af30f2d9d09325fa0
NextZtepS <natdanaiongarjvaja@gmail.com>
2023-08-22 22:55:47 +0700

    adjust primary grid

--- src/lib/components/utils/GridPrimary.svelte
+++ src/lib/components/utils/GridPrimary.svelte
@@ -1,3 +1,3 @@
-<main class="grid gap-4 lg:gap-8 p-5 lg:px-12 lg:py-6 lg:grid-cols-2 2xl:grid-cols-3 4xl:grid-cols-4">
+<main class="grid gap-4 lg:gap-8 p-5 lg:px-12 lg:py-6 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
     <slot />
 </main>
